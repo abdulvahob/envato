f8d100ab203e6d4cf6cccaff02cb06a234fb1ef8
Hakiton <abdulvahobabdullayev2002@gmail.com>
2022-04-09 22:48:52 +0500

    add media

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 new WOW().init();
+
+
+let link  = document.querySelectorAll(".nav-link");
+let section = document.querySelectorAll("section");
+
+window.onscroll = ()=>{
+   section.forEach(sec => {
+      let  top  =  window.scrollY;
+      let offset  = sec.offsetTop-150;
+      let height  =  sec.offsetHeight;
+      let id = sec.getAttribute("id");
+
+      if(top >= offset && top < offset + height){
+            link.forEach(links =>{
+               links.classList.remove("active");
+               document.querySelector("nav ul li  a[href*=" + id + "]").classList.add('active')
+            })
+
+      }
+   })
+}
